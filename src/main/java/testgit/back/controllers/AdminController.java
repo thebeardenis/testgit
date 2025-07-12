ce2979f97398bd76fb16599b864dd745fa9278be
thebeardenis <denis.kolesnik999111@gmail.com>
2025-07-12 10:56:04 +0300

    leetcode

--- src/main/java/testgit/back/controllers/AdminController.java
+++ src/main/java/testgit/back/controllers/AdminController.java
@@ -11,12 +11,32 @@ import java.util.List;
 public class AdminController {
     private User user;
     public String calculateStats(String input) {
-        int upper = 0;
-        int lovest = 0;
-        int zero = 0;
-        List<String> a = List.of(input.split(" "));
-        int[] nums = new int[] {};
+        return "";
+    }
+}
 
-        return "выше нуля: " + upper + ", ниже нуля: " + lovest + ", равна нулю: " + zero;
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int lst = digits.length - 1;
+        for (int i=lst; i >= 0; i--) {
+            if (digits[i] <= 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else {
+                if (i-1 >= 0) {
+                    digits[i] = 0;
+                    digits[i-1] = digits[i-1] + 1;
+                }
+            }
+        }
+        if (digits[0] > 9) {
+            int[] res = new int[lst+2];
+            res[0] = 1;
+            for (int i=1; i <= lst+1; i++) {
+                res[i] = digits[i-1]%10;
+            }
+            return res;
+        }
+        return digits;
     }
 }
